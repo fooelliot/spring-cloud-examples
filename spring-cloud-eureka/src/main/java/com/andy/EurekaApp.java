1fdaa258b6f7af818bff48eb71d0e6443f8e2e93
janlle <rethinkt@163.com>
2018-06-20 00:29:31 +0800

    update some file

--- spring-cloud-eureka/src/main/java/com/andy/EurekaApp.java
+++ spring-cloud-eureka/src/main/java/com/andy/EurekaApp.java
@@ -3,6 +3,9 @@ package com.andy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableEurekaServer
 @SpringBootApplication
@@ -10,4 +13,13 @@ public class EurekaApp {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApp.class, args);
 	}
+
+	@EnableWebSecurity
+	static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable();
+		}
+	}
+
 }
