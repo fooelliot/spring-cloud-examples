eaf5bab810acb26e2c4b660e7c262cf720898ae5
janlle <rethinkt@163.com>
2018-06-23 00:17:29 +0800

    update some code

--- spring-cloud-config-server/src/main/java/com/andy/config/ConfigServerApp.java
+++ spring-cloud-config-server/src/main/java/com/andy/config/ConfigServerApp.java
@@ -3,7 +3,6 @@ package com.andy.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApp {
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApp.class, args);
-//		System.out.println(System.getProperty("user.dir"));
-	}
-	@RequestMapping("/")
-	public String home() {
-		return "Hello World!";
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApp.class, args);
+        System.out.println(System.getProperty("user.dir"));
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
 
 }
