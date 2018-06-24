65b4b8cc7def424e43238ef4d38a6bb447b4644d
lyon <rethinkt@163.com>
2018-06-24 10:55:59 +0800

    update some code

--- spring-cloud-config-server-bus/src/main/java/com/andy/config/ConfigServerBusApp.java
+++ spring-cloud-config-server-bus/src/main/java/com/andy/config/ConfigServerBusApp.java
@@ -3,11 +3,13 @@ package com.andy.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @Author: Mr.lyon
  * @CreateBy: 2018-2-23 16:50
  **/
+@EnableConfigServer
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServerBusApp {
