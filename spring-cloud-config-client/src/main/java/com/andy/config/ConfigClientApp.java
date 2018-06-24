b45ea1ecadfdde3d6f33adc9f2e7c561f4be5d04
lyon <rethinkt@163.com>
2018-06-24 13:36:24 +0800

    update some code

--- spring-cloud-config-client/src/main/java/com/andy/config/ConfigClientApp.java
+++ spring-cloud-config-client/src/main/java/com/andy/config/ConfigClientApp.java
@@ -2,11 +2,13 @@ package com.andy.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @Author: Mr.lyon
  * @CreateBy: 2018-1-23 10:12
  **/
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigClientApp {
     public static void main(String[] args) {
