c8f15ae5c9f060f58d29d3f2c0505c849c447ea1
lyon <rethinkt@163.com>
2018-07-02 22:23:09 +0800

    update some code

--- spring-cloud-hystrix-dashboard/src/main/java/com/andy/dashboard/HystrixDashboardApp.java
+++ spring-cloud-hystrix-dashboard/src/main/java/com/andy/dashboard/HystrixDashboardApp.java
@@ -1,11 +1,11 @@
-package com.andy;
+package com.andy.dashboard;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableHystrixDashboard
-@SpringBootApplication
+@SpringCloudApplication
 public class HystrixDashboardApp {
 	public static void main(String[] args) {
 		SpringApplication.run(HystrixDashboardApp.class, args);
