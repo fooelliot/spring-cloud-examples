aafca217e3534ef340f9b93a0a4aae9da098a22d
lyon <rethinkt@163.com>
2018-07-03 22:25:56 +0800

    rename some model

--- spring-cloud-hystrix-dashboard/src/main/java/com/andy/dashboard/DashboardApp.java
+++ spring-cloud-hystrix-dashboard/src/main/java/com/andy/dashboard/DashboardApp.java
@@ -8,10 +8,10 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 @EnableHystrix
 @EnableHystrixDashboard
 @SpringCloudApplication
-public class HystrixDashboardApp {
+public class DashboardApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(HystrixDashboardApp.class, args);
+        SpringApplication.run(DashboardApp.class, args);
     }
 
 
