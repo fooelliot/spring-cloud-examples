1049ad9bc5f7e1cdd28521a4f4d4aa18f14f1557
lyon <rethinkt@163.com>
2018-07-15 10:30:46 +0800

    update some code

--- spring-cloud-dashboard/src/main/java/com/andy/dashboard/DashboardApp.java
+++ spring-cloud-dashboard/src/main/java/com/andy/dashboard/DashboardApp.java
@@ -1,10 +1,16 @@
 package com.andy.dashboard;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @EnableHystrix
 @EnableHystrixDashboard
 @SpringCloudApplication
@@ -14,5 +20,19 @@ public class DashboardApp {
         SpringApplication.run(DashboardApp.class, args);
     }
 
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "success";
+    }
 
 }
