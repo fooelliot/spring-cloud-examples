7a17629eb00c57a454bd58dbab18a18ef8b29d4d
lyon <rethinkt@163.com>
2018-07-30 22:57:15 +0800

    update some code

--- spring-cloud-bus/src/main/java/com/andy/SpringCloudApp.java
+++ spring-cloud-bus/src/main/java/com/andy/SpringCloudApp.java
@@ -10,6 +10,9 @@ public class SpringCloudApp {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudApp.class, args);
+
+        // 创建 Annotation 驱动的 Spring 应用上下文
+
     }
 
 
