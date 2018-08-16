5f622dede2c86d44eed87161ed16bca9c35f2190
lyon <rethinkt@163.com>
2018-08-16 22:46:43 +0800

    update zuul model code

--- spring-cloud-zuul/src/main/java/com/andy/zuul/ZuulApp.java
+++ spring-cloud-zuul/src/main/java/com/andy/zuul/ZuulApp.java
@@ -3,15 +3,13 @@ package com.andy.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
-import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringCloudApplication
 public class ZuulApp {
-	public static void main(String[] args) {
-		SpringApplication.run(ZuulApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApp.class, args);
+    }
 
 //	@Bean
 //	public PatternServiceRouteMapper serviceRouteMapper() {
